6326fc887a6e8547063e2df7063508db26acce3c
Sidharth Ramkumar <sramk002@wch136-24.cs.ucr.edu>
2021-04-11 12:54:31 -0700

    Completed part 5

---
+++ Lab3_BitManipulation/turnin/sramk002_lab3_part5.c
@@ -0,0 +1,38 @@
+/*	Author: Sidharth Ramkuamar (sramk002@ucr.edu)
+ *  Partner(s) Name: N/A
+ *	Lab Sectiion: 022
+ *	Assignment: Lab #3  Exercise #4
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRD = 0x00; PORTD = 0xFF; //inputs
+    DDRB = 0xFE; PORTB = 0x00; //outputs
+
+    /* Insert your solution below */
+    unsigned char tmpB = 0x01;
+    unsigned char tmpD = 0x00;
+    
+    while (1) {
+	tmpD = PIND << 1;
+	tmpD = tmpD | (PINB & 0x01);
+	
+	if (tmpD >= 0x46){
+	   tmpB = 0x02; //airbag enabled
+	} else if ((tmpD > 0x05) && (tmpD < 0x46)){
+	   tmpB = 0x04; //airbag disabled
+	} else {
+	   tmpB = 0x00; //no passenger
+     }
+    PORTB = tmpB;	
+    }
+    return 1;
+}
